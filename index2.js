f7e874ef3798762ba69230ad50b7e267fd9b575d
Savo Vuksan <savovuksan@gmail.com>
2019-02-01 17:23:54 +0100

    Changed CORS Headers

--- index2.js
+++ index2.js
@@ -15,7 +15,6 @@ const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Origin", "http://localhost:4201");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
